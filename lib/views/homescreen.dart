929aca1f04a49e3341aa4908369d24bb6b593a19
KEREM BAS <kerembas2000@gmail.com>
2022-04-02 18:25:12 +0300

    main

--- lib/views/homescreen.dart
+++ lib/views/homescreen.dart
@@ -27,24 +27,24 @@ class _homepageState extends State<homepage> {
       floatingActionButton: FloatingActionButton(
         backgroundColor: secendcolor,
         onPressed: add,
-        child: Icon(
+        child: const Icon(
           Icons.add,
           size: 40,
         ),
       ),
       backgroundColor: maincolor,
       body: Padding(
-        padding: EdgeInsets.only(top: 30),
+        padding: const EdgeInsets.only(top: 30),
         child: Column(
           children: [
             Padding(
-              padding: EdgeInsets.only(left: 10, right: 10),
+              padding: const EdgeInsets.only(left: 10, right: 10, top: 15),
               child: TextField(
                 cursorColor: secendcolor,
                 decoration: InputDecoration(
                   focusedBorder: OutlineInputBorder(
                     borderRadius: BorderRadius.circular(20),
-                    borderSide: BorderSide(color: secendcolor),
+                    borderSide: const BorderSide(color: secendcolor),
                   ),
                   hintText: "Task",
                   fillColor: textcolor,
@@ -54,7 +54,7 @@ class _homepageState extends State<homepage> {
                     borderRadius: BorderRadius.circular(20),
                   ),
                 ),
-                style: TextStyle(color: secendcolor),
+                style: const TextStyle(color: secendcolor),
                 controller: t1,
               ),
             ),
@@ -62,7 +62,7 @@ class _homepageState extends State<homepage> {
               child: ListView.builder(
                 itemCount: hehe.length,
                 itemBuilder: ((context, index) => Padding(
-                      padding: EdgeInsets.all(8.0),
+                      padding: const EdgeInsets.all(8.0),
                       child: Container(
                         decoration: BoxDecoration(
                           color: secendcolor,
@@ -70,17 +70,17 @@ class _homepageState extends State<homepage> {
                         ),
                         height: 75,
                         child: Padding(
-                          padding: EdgeInsets.only(left: 20, right: 20),
+                          padding: const EdgeInsets.only(left: 20, right: 20),
                           child: Row(
                             mainAxisAlignment: MainAxisAlignment.spaceBetween,
                             children: [
                               Text(
                                 hehe[index],
-                                style: TextStyle(color: textcolor),
+                                style: const TextStyle(color: textcolor),
                               ),
                               IconButton(
                                 color: Colors.red,
-                                icon: Icon(Icons.remove),
+                                icon: const Icon(Icons.remove),
                                 onPressed: () {
                                   setState(() {
                                     hehe.removeAt(index);
