5678a037033601f2607fef6c69ed6a6d5cd796dc
KEREM BAS <kerembas2000@gmail.com>
2022-11-04 21:42:07 +0300

    main

--- lib/views/homescreen.dart
+++ lib/views/homescreen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'package:todo/config/colors.dart';
 
 class homepage extends StatefulWidget {
